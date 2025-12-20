924666c3a937f804ac2c8124f35b9f3a085816d9
Yuri Melo <yurifullstack@gmail.com>
2025-12-20 14:48:19 -0300

    refactor: update LSP configuration to use new API and improve keymap setup

--- lua/plugins/lsp/mason-config.lua
+++ lua/plugins/lsp/mason-config.lua
@@ -28,16 +28,36 @@ function Mason.GetNvimLspConfig()
     {
       "neovim/nvim-lspconfig",
       config = function()
-        local lspconfig = require("lspconfig")
+        -- Nova API do Neovim 0.11+
         local omnisharpConfiguration = require("plugins.lsp.omnisharp")
 
-        lspconfig.lua_ls.setup({})
-        lspconfig.ts_ls.setup({})
-        lspconfig.omnisharp.setup(omnisharpConfiguration.GetBaseConfig())
+        -- Configurar servidores com a nova API
+        vim.lsp.config("lua_ls", {
+          root_markers = { ".luarc.json", ".luarc.jsonc", ".stylua.toml", "stylua.toml", "selene.yml", ".git" },
+        })
 
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+        vim.lsp.config("ts_ls", {
+          root_markers = { "tsconfig.json", "package.json", ".git" },
+        })
+
+        -- OmniSharp com configurações customizadas
+        vim.lsp.config("omnisharp", omnisharpConfiguration.GetBaseConfig())
+
+        -- Ativar automaticamente os servidores configurados
+        vim.lsp.enable({ "lua_ls", "ts_ls", "omnisharp" })
+
+        -- Keymaps globais para LSP
+        local function setup_keymaps()
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = true, desc = "Hover" })
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true, desc = "Goto definition" })
+          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { buffer = true, desc = "Code action" })
+        end
+
+        -- Aplicar keymaps quando um servidor LSP inicia
+        vim.api.nvim_create_autocmd("LspAttach", {
+          callback = setup_keymaps,
+          group = vim.api.nvim_create_augroup("lsp_attach", { clear = true }),
+        })
       end
     }
 end
