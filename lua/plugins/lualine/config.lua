92588f018df87ea00116f61c3f6d59e907fef91e
Yuri Melo <yurifullstack@gmail.com>
2025-12-21 11:20:32 -0300

    add config

--- lua/plugins/lualine/config.lua
+++ lua/plugins/lualine/config.lua
@@ -1,16 +1,17 @@
 local Lualine = {}
 
 function Lualine.GetLualineConfig()
-    return
-        {
-            'nvim-lualine/lualine.nvim',
-            requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-            config = function()
-                require('lualine').setup({
-                    theme = "gruvbox"
-                })
-            end
-        }
+  return
+    {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+      config = function()
+        require('lualine').setup({
+          theme = "gruvbox"
+        })
+      end
+    }
 end
 
 return Lualine
+
