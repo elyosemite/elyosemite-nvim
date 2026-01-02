da0353291deaaadb3783c803ece0e07a433318af
Yuri Melo <yurifullstack@gmail.com>
2026-01-02 12:18:32 -0300

    refactor: remove commented-out setup options for nvim-autopairs plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,8 +30,6 @@ Plugins.all_plugins = {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
   }
 }
 
